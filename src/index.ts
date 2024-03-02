a5dd7083160f28e07c7dd2651c2f8e881d5b1079
Bruce Webster <brucewebster222@gmail.com>
2024-03-02 10:02:10 -0400

    Connected MongoDB database using mongoose

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -21,3 +22,10 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log("Server is running on http://localhost:8080/");
 });
+
+const MONGO_URL =
+  "mongodb+srv://brucewebster222:brucewebster222@cluster0.v9i4afx.mongodb.net/";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
